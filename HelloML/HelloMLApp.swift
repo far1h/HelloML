6782ba05983599dadbd2840a85bf2e7e70a27523
far1h <farihmuhammad45@gmail.com>
2025-10-20 00:08:53 +0700

    Initial Commit

---
+++ HelloML/HelloMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMLApp.swift
+//  HelloML
+//
+//  Created by Farih Muhammad on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
