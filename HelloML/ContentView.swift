8ffe5e6240e5f72fd912b0ff52fcf7ad152ea255
far1h <farihmuhammad45@gmail.com>
2025-10-20 08:16:49 +0700

    Implement User Interface

--- HelloML/ContentView.swift
+++ HelloML/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let images = ["1", "2", "3"]
+    @State private var currentIndex = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(images[currentIndex])
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            HStack {
+                Button("Previous") {
+                    currentIndex = currentIndex - 1
+                }.disabled(currentIndex == 0)
+                Button("Next") {
+                    currentIndex = currentIndex + 1
+                }.disabled(currentIndex == images.count - 1)
+                
+            }.buttonStyle(.bordered)
+            Button("Predict") {
+                // Prediction logic goes here              
+            }.buttonStyle(.borderedProminent)
+            
+            List {
+                Text("Prediction 1")
+                Text("Prediction 2")
+                Text("Prediction 3")
+            }
         }
-        .padding()
     }
 }
 
